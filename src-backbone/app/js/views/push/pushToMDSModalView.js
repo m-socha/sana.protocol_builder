37ab24a51554d6f01f34f9c34e6707a70379ad74
Michael Socha <msocha@uwaterloo.ca>
2018-10-09 20:37:48 -0700

    Add modal for adding MDS endpoints.

---
+++ src-backbone/app/js/views/push/pushToMDSModalView.js
@@ -0,0 +1,46 @@
+const ModalLayoutView = require('views/common/modalLayoutView');
+
+
+module.exports = Marionette.LayoutView.extend({
+    template: require('templates/push/pushToMDSModalView'),
+
+    // regions: {
+    //     'body': 'section#modal-body',
+    // },
+
+    events: {
+        'click a#submit-btn': '_onSubmit',
+    },
+
+    ui: {
+        urlField: 'input#mds-endpoint',
+        usernameField: 'input#mds-username',
+        passwordField: 'input#mds-password',
+    },
+
+    templateHelpers: function() {
+        return {
+            headingText: this.headingText,
+            url: this.url,
+            username: this.username,
+            password: this.password,
+        };
+    },
+
+    initialize: function(options) {
+        this.headingText = options.headingText;
+        this.url = options.url;
+        this.username = options.username;
+        this.password = options.password;
+
+        this.onSubmit = options.onSubmit;
+    },
+
+    _onSubmit: function() {
+        const url = this.ui.urlField.val();
+        const username = this.ui.usernameField.val();
+        const password = this.ui.passwordField.val();
+
+        this.onSubmit(url, username, password);
+    },
+});
